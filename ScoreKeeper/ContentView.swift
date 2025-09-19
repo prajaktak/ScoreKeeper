75e69b57a14f65594397fa1a68c85d9475f2453f
Prajakta Kulkarni <your.email@example.com>
2025-09-19 15:51:34 +0200

    Created a list using static variables to show player names and their scores, created a button which will add player name and that player's score to the list

--- ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ContentView.swift
@@ -8,14 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var players: [String] = ["Elisa", "Juan", "Pedro"]
+    @State private var scores: [Int] = [0,0,0]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(0..<players.count, id:\.description){index in
+                TextField("Name", text: $players[index])
+                Stepper("\(scores[index])", value: $scores[index])
+            }
+            Button("Add player",systemImage: "plus.circle"){
+                players.append("")
+                scores.append(0)
+            }
         }
         .padding()
+        Spacer()
     }
 }
 
